02f4ed60ae28994809d25a3e4589a0cea19eca7a
bimaoy <bima.olga@midasteknologi.com>
2021-12-22 17:13:11 +0700

    add function to calculate 3 biggest buyers

--- fn_top5_buyer.sql
+++ fn_top5_buyer.sql
@@ -12,3 +12,50 @@ ORDER BY
    total_transaksi desc
 FETCH FIRST 3 ROW ONLY;
 /
+
+--SELECT TOP 3 BUYYER WITH FUNCTION
+--create an object type to hold multiple attribute
+create or replace type top3_buyer_obj_type as object
+(
+op_buyer_id number,
+op_nama_pelanggan varchar2(200),
+op_tot_transaksi number
+);
+/
+
+--create a nested table based on object created, will be perfect to fetch data
+create or replace type top3_buyer_tbl_type
+is table of top3_buyer_obj_type;
+/
+
+drop type top3_buyer_obj_type;
+/
+
+drop type top3_buyer_tbl_type;
+/
+
+create or replace function fn_top3_buyer
+return top3_buyer_tbl_type
+IS
+    --nested table variable declaration and initialization
+    top3_buyer_nt top3_buyer_tbl_type := top3_buyer_tbl_type();
+BEGIN
+    --extending the nested table    
+    top3_buyer_nt.extend();
+    --get the required data into variable
+    SELECT top3_buyer_obj_type(buyer_id, nama_user, sum(total))
+    bulk collect into top3_buyer_nt
+    FROM
+       ecm_orders a
+       JOIN ecm_users b ON a.buyer_id = b.user_id
+    GROUP BY
+       buyer_id, nama_user
+    ORDER BY
+       sum(total) desc
+    FETCH FIRST 3 ROW ONLY;
+    return top3_buyer_nt;
+END;
+/
+
+select * from table (fn_top3_buyer);
+/
