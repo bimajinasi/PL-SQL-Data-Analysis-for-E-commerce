e6200dfe4c57f3ad20f130607d97c9bca24f3eea
bimaoy <bima.olga@midasteknologi.com>
2021-12-22 17:16:14 +0700

    add function to calculate total buyer and seller with nested table and bulk collection

--- fn_transaction_buyer_and_seller.sql
+++ fn_transaction_buyer_and_seller.sql
@@ -49,3 +49,44 @@ SELECT fn_trn_sell FROM DUAL;
 --execute 2 function
 SELECT fn_trn_buy_sell, fn_trn_sell FROM DUAL;
 /
+
+
+--=====================================--
+--CREATE FUNCTION V.2 with NESTED TABLE
+--create an object type to hold multiple attribute
+create or replace type buy_sell_obj_type as object
+(
+op_tot_seller number,
+op_tot_buyer number
+);
+/
+
+--create a nested table based on object created, will be perfect to fetch data
+create or replace type buy_sell_tbl_type
+is table of buy_sell_obj_type;
+/
+
+drop type buy_sell_obj_type;
+/
+
+drop type buy_sell_tbl_type;
+/
+
+create or replace function fn_tot_buy_sell
+return buy_sell_tbl_type
+IS
+    --nested table variable declaration and initialization
+    tot_buy_sell buy_sell_tbl_type := buy_sell_tbl_type();
+BEGIN
+    --extending the nested table    
+    tot_buy_sell.extend();
+    --get the required data into variable
+    SELECT buy_sell_obj_type(count(distinct seller_id), count(distinct buyer_id))
+    bulk collect into tot_buy_sell
+    from ecm_orders;
+    return tot_buy_sell;
+END;
+/
+
+select * from table (fn_tot_buy_sell);
+/
